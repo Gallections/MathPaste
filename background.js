fbc045fd910a8447ad0485af245bcc9d069b1a04
Gallections <Gallections@gmail.com>
2025-05-25 14:55:17 -0700

    fix: fixing content injection indeterminism bug

--- background.js
+++ background.js
@@ -2,7 +2,65 @@ let extensionEnabled = false;
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.create({ url: chrome.runtime.getURL("onboarding.html") });
-  });
+});
+
+
+// Outdated Code: Rather than performing checks on active tabs, we inject script every time user changes active tabs
+// chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+//     if (tabs.length === 0) {
+//         console.warn('No active tab found.');
+//         return;
+//     }
+
+//     const tabId = tabs[0].id;
+
+//     chrome.tabs.sendMessage(tabId, { type: 'ping' }, response => {
+//         if (chrome.runtime.lastError) {
+//             console.warn('Content script missing, injecting.');
+//             injectContentScript(tabId);
+//         } else {
+//             console.log('Content script alive, proceeding.');
+//             // chrome.tabs.sendMessage(tabId, { type: 'setup' });
+//         }
+//     });
+// });
+
+
+chrome.tabs.onActivated.addListener((activeTab)=> {
+    chrome.tabs.get(activeTab.tabId, tab => {
+        if (isInjectableUrl(tab.url)) {
+            injectContentScript(tab.id);
+            console.log("Injection Successful on new active Tab!")
+        }
+    });
+})
+
+chrome.webNavigation.onCompleted.addListener(details => {
+    chrome.tabs.get(details.tabId, tab => {
+        if (isInjectableUrl(tab.url)) {
+            injectContentScript(tab.id);
+            console.log("Injection Successful on a newly navigated page")
+        }
+    });
+});
+
+function isInjectableUrl(url) {
+    if (!url) return false;
+    return url.startsWith('http://') || url.startsWith('https://');
+} 
+
+
+function injectContentScript(tabId) {
+    chrome.scripting.executeScript({
+        target: { tabId },
+        files: ['copy.js']
+    }).then(() => {
+        console.log('Injected content script into tab', tabId);
+        // chrome.tabs.sendMessage(tabId, { type: 'setup' });
+    }).catch(err => {
+        console.error('Failed to inject content script:', err);
+    });
+}
   
 
 chrome.commands.onCommand.addListener((command)=> {
